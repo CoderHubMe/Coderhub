2d83d5c4b4d861b956959b71b7ce5164fd2b81db
percyodi <pearse.2008@gmail.com>
2016-04-30 22:58:54 +0000

    Visualizations with Chart.JS with live Data

---
+++ codeigniter/application/models/Analytics_model.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Analytics_model extends MY_Model {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function skillsChartQuery() {
+        $result = $this->_database
+            ->select('skills.name, COUNT(user_id) as count')
+            ->from('skills')
+            ->join('user_skills', 'skills.id = user_skills.skills_id')
+            ->group_by('skills_id')
+            ->order_by('count', 'DESC')
+            ->get()
+            ->{$this->_return_type(1)}();
+            
+        return $result;
+    }
+    
+    public function connAndBlockedConnOverTime() {
+        $result = $this->_database
+            ->select("IF(is_accepted = 1, CONCAT(MONTHNAME(date_accepted), ' ', YEAR(date_accepted)), CONCAT(MONTHNAME(date_blocked), ' ', YEAR(date_blocked))) as label,
+                COUNT(IF(is_accepted = 1, 1, null)) as count_accepted,
+                COUNT(IF(is_blocked = 1, 1, null)) as count_blocked")
+            ->from('connections')
+            ->group_start()
+                ->where('(is_accepted', '1')
+                ->where('date_accepted is not null')
+            ->group_end()
+            ->or_group_start()
+                ->where('is_blocked', '1')
+                ->where("date_blocked is not null")
+            ->group_end()
+            ->group_end() // IDK why, but this is required for proper parentheses - Pearse
+            ->group_by('label')
+            ->order_by('label')
+            ->get()
+            ->{$this->_return_type(1)}();
+            
+        return $result;
+    }
+}
--- codeigniter/application/views/home/index.php
+++ codeigniter/application/views/home/index.php
@@ -1,3 +1,134 @@
-<h1>Dump of $_SESSION Data</h1>
-<?php
-var_dump($_SESSION);
+<div class="row">
+  <div class="col-md-6">
+    <!-- DONUT CHART -->
+    <div class="box box-danger">
+      <div class="box-header with-border">
+        <h3 class="box-title">User Skills</h3>
+    
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+      </div>
+      <div class="box-body">
+        <canvas id="userSkillsChart" style="height:250px"></canvas>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <script>
+    $(document).ready(function() {
+      $.getJSON("<?= base_url('analytics/skillsChart') ?>", function(jsonData) {
+        console.dir(jsonData);
+        var ctx = $('#userSkillsChart');
+        var data = {
+            labels: jsonData.labels,
+            datasets: [{
+              data: jsonData.data,
+              backgroundColor: jsonData.colors,
+              hoverBackgroundColor: jsonData.colors
+            }]
+        };
+        var myDoughnutChart = new Chart(ctx, {
+          type: 'doughnut',
+          data: data,
+          options: {
+            type:"doughnut",
+            animation:{
+              animateScale: true,
+              animateRotate: false
+            }
+          }
+        });
+      });
+    });
+  </script>
+  <div class="col-md-6">
+    <!-- LINE CHART -->
+    <div class="box box-info">
+      <div class="box-header with-border">
+        <h3 class="box-title">Connections and Blocked Connections Over Time</h3>
+    
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+      </div>
+      <div class="box-body">
+        <div class="chart">
+          <canvas id="connAndBlockedConnOverTimeChart" style="height:250px"></canvas>
+        </div>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <script>
+    $(document).ready(function() {
+      $.getJSON("<?= base_url('analytics/connAndBlockedConnOverTime') ?>", function(jsonData) {
+        console.dir(jsonData);
+        var ctx = $('#connAndBlockedConnOverTimeChart');
+        var data = {
+            labels: jsonData.labels,
+            datasets: [{
+              label: "Accepted Connections",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: jsonData.accepted_data,
+            },
+            {
+              label: "Blocked Connections",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "rgba(255, 67, 67,0.4)",
+              borderColor: "rgba(255, 67, 67,1)",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(255, 67, 67,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(255, 67, 67,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: jsonData.blocked_data,
+            }]
+        };
+        var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: data,
+          options: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                stepSize: 1
+              }
+            }]
+          }
+        });
+      });
+    });
+  </script>
+</div>
