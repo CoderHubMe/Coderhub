9456257477e558c5d8dc040382f56cb5288c7b22
percyodi <pearse.2008@gmail.com>
2016-05-01 22:26:27 +0000

    Using www instead of just https

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
-if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
-    header('Location: http'.(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'').'://' . substr($_SERVER['HTTP_HOST'], 4).$_SERVER['REQUEST_URI']);
+if (substr($_SERVER['HTTP_HOST'], 0, 4) !== 'www.') {
+    header('Location: http'.(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'').'://www.' . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
     exit;
 }
 
